368bafaf5aea77b40a841f44348ef22d76b4e285
Tynylii <eemil.suominen@edu.ylojarvi.fi>
2026-05-12 10:11:16 +0300

    possible fix

--- index.js
+++ index.js
@@ -186,17 +186,6 @@ function physicsLoop() {
     requestAnimationFrame(physicsLoop);
 }
 
-function morecubes(amount, cost) {
-    if (moneyamount < cost) return;
-
-    moneyamount -= cost;
-    money.textContent = moneyamount + "$";
-    localStorage.setItem("money", moneyamount);
-
-    for (let i = 0; i < amount; i++) {
-        createCube();
-    }
-}
 
 function reloadmoney() {
   money.textContent = moneyamount + "$";
---
+++ shop.js
@@ -0,0 +1,11 @@
+function morecubes(amount, cost) {
+    if (moneyamount < cost) return;
+
+    moneyamount -= cost;
+    money.textContent = moneyamount + "$";
+    localStorage.setItem("money", moneyamount);
+
+    for (let i = 0; i < amount; i++) {
+        createCube();
+    }
+}
